0d0ca4323a2e782284bf3346f06b227f89f075da
maxwellhertz <maxwellhertz1023@gmail.com>
2023-12-02 15:54:10 +0800

    release v0.0.1

---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+)
+
+const (
+	PmdReleasePkg   = "pmd-bin-6.55.0"
+	PmdReleaseUrl   = "https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.55.0/pmd-bin-6.55.0.zip"
+	PmdLocalPath    = "./target/pmd"
+	PmdLocalBinPath = PmdLocalPath + "/" + PmdReleasePkg + "/" + "bin"
+
+	AppName = "pmd-java-pre-commit-hook"
+)
+
+func main() {
+	stagedJavaFiles, err := getStagedJavaFiles()
+	if err != nil {
+		log.Printf("[WARN] %v failed to get staged java files: %v\n", AppName, err)
+		os.Exit(0)
+	}
+	if len(stagedJavaFiles) == 0 {
+		log.Printf("[INFO] %v didn't find any staged Java files\n", AppName)
+		os.Exit(0)
+	}
+
+	pmdTargetFile := filepath.Join(PmdLocalPath, "sourceFiles.txt")
+	file, err := createFile(pmdTargetFile)
+	if err != nil {
+		log.Printf("[WARN] %v failed to create %v: %v\n", AppName, pmdTargetFile, err)
+		os.Exit(0)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	for _, path := range stagedJavaFiles {
+		writer.WriteString(path + "\n")
+	}
+	writer.Flush()
+
+	ok, pmdBinPath := lookupPmd()
+	if !ok {
+		log.Printf("[INFO] %v is installing PMD...\n", AppName)
+		pmdBinPath, err = installPmd()
+		if err != nil {
+			log.Printf("[WARN] %v failed to install PMD: %v\n", AppName, err)
+			os.Exit(0)
+		}
+	}
+
+	pmdCmd := exec.Command(filepath.Join(pmdBinPath, getPmdScript()), getPmdCommand(), "-f", "text", "-R", "rulesets/java/quickstart.xml", "--cache", filepath.Join(PmdLocalPath, "cache"), "--file-list", pmdTargetFile)
+	pmdCmd.Stdout = os.Stdout
+	pmdCmd.Stderr = os.Stderr
+	err = pmdCmd.Run()
+	if err == nil {
+		os.Exit(0)
+	}
+
+	fmt.Print("PMD reported error! Still commit the changes? [y/N]: ")
+	var input string
+	fmt.Scan(&input)
+	if strings.TrimSpace(input) == "N" {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func getStagedJavaFiles() ([]string, error) {
+	repo, err := git.PlainOpen(".")
+	if errors.Is(err, git.ErrRepositoryNotExists) {
+		return make([]string, 0), nil
+	}
+	workTree, err := repo.Worktree()
+	if err != nil {
+		return make([]string, 0), err
+	}
+	status, err := workTree.Status()
+	if err != nil {
+		return make([]string, 0), err
+	}
+
+	stagedJavaFiles := make([]string, 0, len(status))
+	for srcFilePath, fileStatus := range status {
+		if fileStatus.Staging != git.Unmodified && fileStatus.Staging != git.Untracked && filepath.Ext(srcFilePath) != ".java" {
+			continue
+		}
+		stagedJavaFiles = append(stagedJavaFiles, srcFilePath)
+	}
+	if len(stagedJavaFiles) == 0 {
+		return make([]string, 0), nil
+	}
+	return stagedJavaFiles, nil
+}
+
+func lookupPmd() (bool, string) {
+	pmdPath, err := exec.LookPath("pmd")
+	if err == nil {
+		return true, pmdPath
+	}
+	fileInfo, err := os.Stat(PmdLocalBinPath)
+	if os.IsNotExist(err) {
+		return false, ""
+	}
+	if fileInfo != nil {
+		return true, PmdLocalBinPath
+	}
+	return false, ""
+}
+
+func installPmd() (string, error) {
+	resp, err := http.Get(PmdReleaseUrl)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	pmdZip := PmdLocalPath + "pkg"
+	out, err := createFile(pmdZip)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		out.Close()
+		os.Remove(pmdZip)
+	}()
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	unzip := exec.Command("unzip", pmdZip, "-d", PmdLocalPath)
+	if err := unzip.Run(); err != nil {
+		return "", err
+	}
+	return filepath.Join(PmdLocalPath, PmdReleasePkg, "bin"), nil
+}
+
+func createFile(path string) (*os.File, error) {
+	err := os.MkdirAll(filepath.Dir(path), 0755)
+	if err != nil {
+		return nil, err
+	}
+	return os.Create(path)
+}
+
+func getPmdScript() string {
+	switch os.Getenv("OS") {
+	case "Windows_NT":
+		return "pmd.bat"
+	default:
+		return "run.sh"
+	}
+}
+
+func getPmdCommand() string {
+	switch os.Getenv("OS") {
+	case "Windows_NT":
+		return ""
+	default:
+		return "pmd"
+	}
+}
